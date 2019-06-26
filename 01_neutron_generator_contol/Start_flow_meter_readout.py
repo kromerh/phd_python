b329cc44295c18043cda20c0b9cd2ef1027c0f5c
heiko.kromer@gmail.com <hkromer@psivpn200.psi.ch>
2019-06-26 19:36:50 +0200

    truncate control table

---
+++ 01_neutron_generator_contol/Start_flow_meter_readout.py
@@ -0,0 +1,23 @@
+import paramiko
+import sys
+host="twofast-RPi3-4"
+user="pi"
+pwd="axfbj1122!"
+paramiko.util.log_to_file('ssh.log') # sets up logging
+
+try:
+    print('Starting flow-meter readout...')
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.load_system_host_keys()
+    client.connect(host, username=user, password=pwd)
+    stdin, stdout, stderr = client.exec_command('python3 /home/pi/Documents/flow_meter/dash_NG/flow_meter/flow_meter_readout_V0.py', get_pty=True)
+    print(stdout.readlines())
+    print(stderr.read())
+    client.close()
+except KeyboardInterrupt:
+    print('Ctrl + C. Exiting. Flushing serial connection.')
+    client.close()
+    sys.exit(1)
+finally:
+    client.close()
