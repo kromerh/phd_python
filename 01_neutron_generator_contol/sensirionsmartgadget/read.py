84cee33b103341a4ca079e5fe3a78303356bce6c
Heiko Kromer <heiko.kromer@gmail.com>
2019-08-27 15:57:15 +0200

    sensirion

--- 01_neutron_generator_contol/sensirionsmartgadget/read.py
+++ 01_neutron_generator_contol/sensirionsmartgadget/read.py
@@ -276,7 +276,7 @@ def main():
         data.reset_index(inplace=True)
         data.rename(columns={"index": "utc_time"}, inplace=True)
         # data['time'] = data['utc_time'].apply(lambda x: utc_to_local_time(x))
-        print(data.head())
+        print(data.tail())
         # print(gadget.loggedData) # contains the data sent via notifications
         gadget.setLoggerIntervalMs(1000) # setting a new logger interval will clear all the logged data on the device
         gadget.disconnect()
