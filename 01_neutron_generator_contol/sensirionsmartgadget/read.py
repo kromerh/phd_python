47d45cc08f6269e81e3a80fc86b1fc0e8fa26555
Heiko Kromer <heiko.kromer@gmail.com>
2019-08-27 15:34:05 +0200

    sensirion

--- 01_neutron_generator_contol/sensirionsmartgadget/read.py
+++ 01_neutron_generator_contol/sensirionsmartgadget/read.py
@@ -222,25 +222,25 @@ def main():
     gadget = SHT31(bleAddress)
     print('Connected')
 
-    print('Device name:', gadget.readDeviceName())
+    # print('Device name:', gadget.readDeviceName())
 
-    print('System ID: ', gadget.readSystemId())
-    print('Model number string:', gadget.readModelNumberString())
-    print('Serial number string:', gadget.readSerialNumberString())
-    print('Firmware revision string:', gadget.readFirmwareRevisionString())
-    print('Hardware revision string:', gadget.readHardwareRevisionString())
-    print('Software revision string:', gadget.readSoftwareRevisionString())
-    print('Manufacturer name string:', gadget.readManufacturerNameString())
+    # print('System ID: ', gadget.readSystemId())
+    # print('Model number string:', gadget.readModelNumberString())
+    # print('Serial number string:', gadget.readSerialNumberString())
+    # print('Firmware revision string:', gadget.readFirmwareRevisionString())
+    # print('Hardware revision string:', gadget.readHardwareRevisionString())
+    # print('Software revision string:', gadget.readSoftwareRevisionString())
+    # print('Manufacturer name string:', gadget.readManufacturerNameString())
 
     print('Battery level [%]:', gadget.readBattery())
-    print('Temperature [°C]:', '{:.2f}'.format(gadget.readTemperature()))
-    print('Humidity [%]:', '{:.2f}'.format(gadget.readHumidity()))
+    # print('Temperature [°C]:', '{:.2f}'.format(gadget.readTemperature()))
+    # print('Humidity [%]:', '{:.2f}'.format(gadget.readHumidity()))
 
-    print('LoggerInterval [ms]: ', gadget.readLoggerIntervalMs())
-    gadget.setSyncTimeMs()
-    time.sleep(0.1) # Sleep a bit to enable the gadget to set the SyncTime; otherwise 0 is read when readNewestTimestampMs is used
-    print('OldestTimestampMs [µs]:', gadget.readOldestTimestampMs(), datetime.utcfromtimestamp(gadget.readOldestTimestampMs()/1000).strftime('%Y-%m-%d %H:%M:%S'))
-    print('NewestTimeStampMs [µs]:', gadget.readNewestTimestampMs(), datetime.utcfromtimestamp(gadget.readNewestTimestampMs()/1000).strftime('%Y-%m-%d %H:%M:%S'))
+    # print('LoggerInterval [ms]: ', gadget.readLoggerIntervalMs())
+    # gadget.setSyncTimeMs()
+    # time.sleep(0.1) # Sleep a bit to enable the gadget to set the SyncTime; otherwise 0 is read when readNewestTimestampMs is used
+    # print('OldestTimestampMs [µs]:', gadget.readOldestTimestampMs(), datetime.utcfromtimestamp(gadget.readOldestTimestampMs()/1000).strftime('%Y-%m-%d %H:%M:%S'))
+    # print('NewestTimeStampMs [µs]:', gadget.readNewestTimestampMs(), datetime.utcfromtimestamp(gadget.readNewestTimestampMs()/1000).strftime('%Y-%m-%d %H:%M:%S'))
 
     gadget.readLoggedDataInterval()
 #     gadget.setTemperatureNotification(True) # enable notifications for humidity values; the object will log incoming data into the loggedData variable
