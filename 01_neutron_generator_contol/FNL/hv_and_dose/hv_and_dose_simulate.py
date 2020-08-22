fc3fa5fa34cce0cc2fe326db0f7a5e16eaef7d1e
Heiko Kromer <heiko.kromer@gmail.com>
2020-08-22 12:54:41 +0200

    simulator

--- 01_neutron_generator_contol/FNL/hv_and_dose/hv_and_dose_simulate.py
+++ 01_neutron_generator_contol/FNL/hv_and_dose/hv_and_dose_simulate.py
@@ -8,12 +8,28 @@ from scipy.interpolate import interp1d
 import pandas as pd
 
 
+# Settings for the simulator
+# path
+PATH_CREDENTIALS = '/Users/hkromer/02_PhD/01.github/FNL_Neutron_Generator_Control/credentials.pw'
+HV_CALIB_FILE = '/Users/hkromer/02_PhD/01.github/phd/01_neutron_generator_contol/FNL/hv_and_dose/HV_readout_calibration.txt'
+I_CALIB_FILE = '/Users/hkromer/02_PhD/01.github/phd/01_neutron_generator_contol/FNL/hv_and_dose/I_readout_calibration.txt'
+
+# readout frequency
+FREQUENCY = 1 # sleep time in second
+
+
+# values to save
+HV_VOLTAGE = 3 # V
+HV_CURRENT = 1.5 # V
+
+DOSE_VOLTAGE = 3 # V
+
+VERBOSE = True
 
 
 # read password and user to database
-credentials_file = r'/Users/hkromer/02_PhD/01.github/FNL_Neutron_Generator_Control/credentials.pw'
 
-credentials = pd.read_csv(credentials_file, header=0)
+credentials = pd.read_csv(PATH_CREDENTIALS, header=0)
 user = credentials['username'].values[0]
 pw = credentials['password'].values[0]
 host="localhost"  # your host
@@ -26,18 +42,7 @@ sql_engine = sql.create_engine(connect_string)
 
 
 
-# Settings for the simulator
-# readout frequency
-FREQUENCY = 1 # sleep time in second
 
-
-# values to save
-HV_VOLTAGE = 0.2 # V
-HV_CURRENT = 0.1 # V
-
-DOSE_VOLTAGE = 3 # V
-
-VERBOSE = True
 
 
 def get_experiment_id(sql_engine, verbose=False):
@@ -62,13 +67,13 @@ def saveDB(experiment_id, dose_voltage, HV_current, HV_voltage, verbose=False):
 # calibrate HV voltage
 
 # correct the HV that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
-df_HV_LT = pd.read_csv('/home/pi/Documents/HV_readout_calibration.txt', delimiter="\t")
+df_HV_LT = pd.read_csv(HV_CALIB_FILE, delimiter="\t")
 # print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
 # interpolation function
-interp_HV_voltage = interp1d(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
+interp_HV_voltage = interp1d(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'], fill_value="extrapolate")
 
 # correct the current that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
-df_HV_I_LT = pd.read_csv('/home/pi/Documents/I_readout_calibration.txt', delimiter="\t")
+df_HV_I_LT = pd.read_csv(I_CALIB_FILE, delimiter="\t")
 # print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
 # interpolation function
 interp_HV_current = interp1d(df_HV_I_LT['Current_read'], df_HV_I_LT['HV_current'])
@@ -90,6 +95,6 @@ while True:
     except KeyboardInterrupt:
         print('Ctrl + C. Exiting. Flushing serial connection.')
         sys.exit(1)
-    finally:
+
 
 
