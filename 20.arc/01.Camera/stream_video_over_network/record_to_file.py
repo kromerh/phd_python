965984e4417626cedf835f48bcae52aa7f81f9f0
Heiko Kromer <heiko.kromer@gmail.com>
2020-08-25 10:16:27 +0200

    record_to_file

--- 20.arc/01.Camera/stream_video_over_network/record_to_file.py
+++ 20.arc/01.Camera/stream_video_over_network/record_to_file.py
@@ -1,7 +1,7 @@
 import picamera
 
 camera = picamera.PiCamera()
-camera.resolution = (2592,1944)
+camera.resolution = (800,600)
 camera.start_recording('my_video.h264')
 camera.wait_recording(10)
 camera.stop_recording()
